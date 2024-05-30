3154d689d2b2efb55789ce4918fa6b3096982678
erikasantanaa <erikamariasantanaa@gmail.com>
2024-05-30 17:28:57 -0300

    inicio do projeto popup em js

---
+++ popup/popup.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-close');
+const cleseButton = document.querySelector('popup-close');
+
+button.addEventListener('click', event => {
+    
+})
